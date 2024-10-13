b53dad3cfd0746fed738204823771a3cd47b413e
Andrew <andrew@andrewsmith.org>
2024-10-13 19:39:52 -0400

    Retrieve access token from Blizzard

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,6 +1,28 @@
 import { Request, Response } from "express";
+import dotenv from "dotenv";
 
-const basicGreeting = (req: Request, res: Response) => {
+dotenv.config();
+
+const client_ID = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+
+const headers = new Headers();
+headers.set(
+  "Authorization",
+  "Basic " + Buffer.from(client_ID + ":" + client_secret).toString("base64")
+);
+
+const form = new FormData();
+form.append("grant_type", "client_credentials");
+
+const basicGreeting = async (req: Request, res: Response) => {
+  const response = await fetch("https://oauth.battle.net/token", {
+    method: "POST",
+    body: form,
+    headers,
+  });
+  const data = await response.json();
+  console.log(data);
   res.json({ response: "hello from the API test route!" });
 };
 
